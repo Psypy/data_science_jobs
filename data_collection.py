b45b6dc464daf2dcb73ed84f196cbe19229cdd64
psypy <maximilian.diederich@gmail.com>
2020-10-03 17:56:46 +0200

    Finished data cleaning. Scraped 500 jobs.

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,6 @@
 import glassdoor_scraper as gs
 import pandas as pd
 path = "/home/max/PycharmProjects/GlassDoor/chromedriver/chromedriver"
-df = gs.get_jobs('data scientist', num_jobs=50, verbose=False, path=path, slp_time=5)
+df = gs.get_jobs('data scientist', num_jobs=500, verbose=False, path=path, slp_time=5)
 
 df.to_csv('jobs.csv', index=False)
